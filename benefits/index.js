8ab5287d92447520b7fa9eef29f7786b8c9eb2db
Prashant Bhadauriya <49376347+Prashant-bhadauriya@users.noreply.github.com>
2024-03-19 12:40:56 +0530

    add new images

---
+++ benefits/index.js
@@ -0,0 +1,7 @@
+import card1 from "./card-1.svg";
+import card2 from "./card-2.svg";
+import card3 from "./card-2.svg";
+import card4 from "./card-4.svg";
+import card5 from "./card-5.svg";
+
+export { card1, card2, card3, card4, card5 };
